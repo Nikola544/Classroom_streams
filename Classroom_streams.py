a4f295394e563832d999f4632e4191c8700e9098
Николай <dhyeheydddjeujd>
2024-12-25 18:17:15 +0300

    25/12

--- Classroom_streams.py
+++ Classroom_streams.py
@@ -1,39 +1,33 @@
 import time
-from threading import Thread, Lock
-
-ENEMIES = 100
-
-enemies_lock = Lock()
+from threading import Thread
 
 
 class Knight(Thread):
-    def __init__(self, name, power):
+    def __init__(self, name, power, enemies):
         super().__init__()
         self.name = name
         self.power = power
+        self.enemies = enemies
 
     def run(self):
-        global ENEMIES
         days = 0
 
-        print(f"{self.name}, на нас напали!")
+        print(f"{self.name}, на нас напали! Врагов: {self.enemies}")
 
-        while True:
-            with enemies_lock:
-                if ENEMIES <= 0:
-                    break
-                ENEMIES -= min(self.power, ENEMIES)
-
+        while self.enemies > 0:
+            self.enemies -= self.power
+            self.enemies = max(self.enemies, 0)  # Убедимся, что врагов не меньше нуля
             days += 1
-            print(f"{self.name} сражается {days} день(дня)..., осталось {ENEMIES} воинов.")
+            print(f"{self.name} сражается {days} день(дня)..., осталось {self.enemies} воинов.")
             time.sleep(1)
 
         print(f"{self.name} одержал победу спустя {days} дней(дня)!")
 
+
 if __name__ == "__main__":
 
-    first_knight = Knight("Sir Lancelot", 10)
-    second_knight = Knight("Sir Galahad", 10)
+    first_knight = Knight("Sir Lancelot", 10, 100)
+    second_knight = Knight("Sir Galahad", 5, 50)
 
     first_knight.start()
     second_knight.start()
